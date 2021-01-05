2ac8d19485018fd66b54adbb1791fe74500b5993
Karlduino <karlduino@gmail.com>
2021-01-05 17:24:54 -0600

    add example of button with pull-down resistor

--- button/button_pulldown.py
+++ button/button_pulldown.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 ##############################
-# use of a button
+# use of a button with pull-down resistor
 ##############################
-# don't seem to need to use a resistor (default is to use a built-in pull-up resistor)
 #
-# ground ---- button leg / other leg ---- pin 20
+# pin 20 --------------------|
+#                            |--- button leg / other leg ---- v3.3
+# ground --- 10k resistor ---| 
 #
 # key features:
 #       <https://gpiozero.readthedocs.io/en/stable/api_input.html?highlight=button#gpiozero.Button>
@@ -21,9 +22,13 @@
 from gpiozero import Button
 from time import sleep
 
-button = Button(20)
+# don't _need_ the bounce_time=None
+# pull_up=False if using a pull-down resistor, as here
+button = Button(20, pull_up=False, bounce_time=None) 
+
 while not button.is_pressed:
     print("button is not pressed")
     sleep(1)
+
 print("button was pressed")
 
